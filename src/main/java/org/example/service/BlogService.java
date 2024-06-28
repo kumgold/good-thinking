a4e48251f3f8d7fdbb9be0fc0f651e6f1619b08f
JunghunKum <123260423+kumgold@users.noreply.github.com>
2024-06-28 21:48:18 +0900

    add CRUD examples

---
+++ src/main/java/org/example/service/BlogService.java
@@ -0,0 +1,45 @@
+package org.example.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.example.domain.Article;
+import org.example.dto.AddArticleRequest;
+import org.example.dto.UpdateArticleRequest;
+import org.example.repository.BlogRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class BlogService {
+
+    private final BlogRepository blogRepository;
+
+    public Article save(AddArticleRequest request) {
+        return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll() {
+        return blogRepository.findAll();
+    }
+
+    public Article findById(long id) {
+        return blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Not found: " + id));
+    }
+
+    public void delete(long id) {
+        blogRepository.deleteById(id);
+    }
+
+    @Transactional
+    public Article update(long id, UpdateArticleRequest request) {
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Not found:" + id));
+
+        article.update(request.getTitle(), request.getContent());
+
+        return article;
+    }
+}
--- src/main/java/org/example/test/repository/BlogRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.test.repository;
-
-import org.example.test.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepository extends JpaRepository<Article, Long> {
-}
