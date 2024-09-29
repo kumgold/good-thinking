48cf1af51fcc49476731bb9d4127abc99c4e3e66
JunghunKum <123260423+kumgold@users.noreply.github.com>
2024-09-29 22:29:22 +0900

    init a project

---
+++ hellospring/src/test/java/com/goldcompany/test/hellospring/HellospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goldcompany.test.hellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
