9a7f9d779fd8036f02c31f0c7e9481266c179869
JunghunKum <123260423+kumgold@users.noreply.github.com>
2024-09-29 11:18:58 +0900

    remove project

--- src/main/java/org/example/repository/BlogRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.repository;
-
-import org.example.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepository extends JpaRepository<Article, Long> {
-}
