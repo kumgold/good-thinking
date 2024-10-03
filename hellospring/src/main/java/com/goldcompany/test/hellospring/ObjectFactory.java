9df6be9d6300b6e26dad0bbaf99a05fa1990aa10
JunghunKum <123260423+kumgold@users.noreply.github.com>
2024-10-03 16:28:55 +0900

    Configuration을 가져오는 다른 방법

--- hellospring/src/main/java/com/goldcompany/test/hellospring/ObjectFactory.java
+++ hellospring/src/main/java/com/goldcompany/test/hellospring/ObjectFactory.java
@@ -1,17 +1,19 @@
 package com.goldcompany.test.hellospring;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan
 public class ObjectFactory {
-    @Bean
-    public PaymentService paymentService() {
-        return new PaymentService(exchangeRateProvider());
-    }
-
-    @Bean
-    public ExchangeRateProvider exchangeRateProvider() {
-        return new WebApiExchangeRateProvider();
-    }
+//    @Bean
+//    public PaymentService paymentService() {
+//        return new PaymentService(exchangeRateProvider());
+//    }
+//
+//    @Bean
+//    public ExchangeRateProvider exchangeRateProvider() {
+//        return new WebApiExchangeRateProvider();
+//    }
 }
