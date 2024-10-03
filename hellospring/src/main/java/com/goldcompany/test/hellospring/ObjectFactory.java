db06d327048607da37e5c55762b66ddf486da378
JunghunKum <123260423+kumgold@users.noreply.github.com>
2024-10-03 16:05:51 +0900

    스프링 컨테이너와 의존관계 주입

--- hellospring/src/main/java/com/goldcompany/test/hellospring/ObjectFactory.java
+++ hellospring/src/main/java/com/goldcompany/test/hellospring/ObjectFactory.java
@@ -1,10 +1,16 @@
 package com.goldcompany.test.hellospring;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ObjectFactory {
+    @Bean
     public PaymentService paymentService() {
         return new PaymentService(exchangeRateProvider());
     }
 
+    @Bean
     public ExchangeRateProvider exchangeRateProvider() {
         return new WebApiExchangeRateProvider();
     }
