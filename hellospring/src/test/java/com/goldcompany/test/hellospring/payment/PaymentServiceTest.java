7d52dc94ded2994fc2f351f963f8b77a855acb9e
JunghunKum <123260423+kumgold@users.noreply.github.com>
2024-10-06 20:14:41 +0900

    테스트와 DI 1

--- hellospring/src/test/java/com/goldcompany/test/hellospring/payment/PaymentServiceTest.java
+++ hellospring/src/test/java/com/goldcompany/test/hellospring/payment/PaymentServiceTest.java
@@ -14,19 +14,21 @@ class PaymentServiceTest {
 
     @Test
     void prepare() throws IOException {
-        PaymentService paymentService = new PaymentService(new WebApiExchangeRateProvider());
-
-       Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
-
-        // 환율정보
-        Assertions.assertThat(payment.getExchangedRate()).isNotNull();
-
-        // 원화환산금액 계산
-        Assertions.assertThat(payment.getConvertedAmount())
-                .isEqualTo(payment.getExchangedRate().multiply(payment.getForeignCurrencyAmount()));
+        getPayment(BigDecimal.valueOf(1314), BigDecimal.valueOf(1_3140));
+        getPayment(BigDecimal.valueOf(1340), BigDecimal.valueOf(1_3400));
+        getPayment(BigDecimal.valueOf(1321), BigDecimal.valueOf(1_3210));
 
         // 원화환산금액 유효시간 계산
-        Assertions.assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
-        Assertions.assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
+//        Assertions.assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
+//        Assertions.assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
+    }
+
+    private static void getPayment(BigDecimal exchangeRate, BigDecimal convertedAmount) throws IOException {
+        PaymentService paymentService = new PaymentService(new ExchangeRateProviderStub(exchangeRate));
+
+        Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
+
+        Assertions.assertThat(payment.getExchangedRate()).isEqualByComparingTo(exchangeRate);
+        Assertions.assertThat(payment.getConvertedAmount()).isEqualByComparingTo(convertedAmount);
     }
 }
