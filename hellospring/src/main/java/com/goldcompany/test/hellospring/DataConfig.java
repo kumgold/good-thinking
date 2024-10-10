5290c6ffd371c7f1fb22db08432c932a709e4040
JunghunKum <123260423+kumgold@users.noreply.github.com>
2024-10-10 23:30:05 +0900

    JPA를 이용한 Order 저장

---
+++ hellospring/src/main/java/com/goldcompany/test/hellospring/DataConfig.java
@@ -0,0 +1,34 @@
+package com.goldcompany.test.hellospring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataConfig {
+    @Bean
+    DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+
+        emf.setDataSource(dataSource());
+        emf.setPackagesToScan("com.goldcompany.test.hellospring");
+        emf.setJpaVendorAdapter(new HibernateJpaVendorAdapter() {{
+            setDatabase(Database.H2);
+            setGenerateDdl(true);
+            setShowSql(true);
+        }});
+
+        return emf;
+    }
+}
