10f629da68c0ad64a869167d05220cc73acdbbaf
JunghunKum <123260423+kumgold@users.noreply.github.com>
2024-10-12 11:18:56 +0900

    DataAccessException과 예외 추상화

--- hellospring/src/main/java/com/goldcompany/test/hellospring/DataClient.java
+++ hellospring/src/main/java/com/goldcompany/test/hellospring/DataClient.java
@@ -5,6 +5,9 @@ import com.goldcompany.test.hellospring.order.Order;
 import jakarta.persistence.EntityManagerFactory;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.math.BigDecimal;
 
@@ -12,8 +15,20 @@ public class DataClient {
     public static void main(String[] args) {
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(DataConfig.class);
         OrderRepository repository = beanFactory.getBean(OrderRepository.class);
+        JpaTransactionManager transactionManager = beanFactory.getBean(JpaTransactionManager.class);
 
-        Order order = new Order("100", BigDecimal.TEN);
-        repository.save(order);
+        try {
+            new TransactionTemplate(transactionManager).execute( status -> {
+                Order order = new Order("100", BigDecimal.TEN);
+                repository.save(order);
+
+                Order order2 = new Order("100", BigDecimal.ONE);
+                repository.save(order2);
+
+                return null;
+            });
+        } catch (DataIntegrityViolationException e) {
+            
+        }
     }
 }
